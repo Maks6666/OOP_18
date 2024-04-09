a73d69de32301519f0edeeaf94809c577c9c5504
Maks Kucher <118250600+Maks6666@users.noreply.github.com>
2024-04-09 11:15:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 # Метаклас, що може змінювати ім'я класу залежно
 # від певних умов або параметрів.
 
-class DynamicNameMeta(type):
+class NameMetaClass(type):
     def __new__(cls, name, bases, dct):
         if 'original_name' in dct:
             name = dct['original_name']
             print(dct)
         return super().__new__(cls, name, bases, dct)
 
-class MyClass(metaclass=DynamicNameMeta):
+class MyClass(metaclass=NameMetaClass):
     ...
 
 print(MyClass.__name__)
 
-class NewClass(metaclass=DynamicNameMeta):
+class NewClass(metaclass=NameMetaClass):
     original_name = "New_Class"
 
 print(NewClass.__name__)
