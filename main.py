23f0f8392bb9beac20d59b8cc0127863ec4a4ee3
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-09 11:03:01 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-# Метаклас, який вносить додаткові перевірки/логіку
-# до певних методів у всіх класах.
+# Метаклас, що може змінювати ім'я класу залежно
+# від певних умов або параметрів.
 
-class NewMetaClass(type):
-    methods = ["add"]
-
+class DynamicNameMeta(type):
     def __new__(cls, name, bases, dct):
-        for method in cls.methods:
-            if method in dct:
-                original_method = dct[method]
-                def new_method(self, x, y):
-                    if y == 0:
-                        raise ZeroDivisionError("Y cannot be 0!")
-                    return original_method(self, x, y)
-                dct[method] = new_method
+        if 'original_name' in dct:
+            name = dct['original_name']
+            print(dct)
         return super().__new__(cls, name, bases, dct)
 
+class MyClass(metaclass=DynamicNameMeta):
+    ...
 
-class MyClass(metaclass=NewMetaClass):
-    def divide(self, x, y):
-        return x / y
+print(MyClass.__name__)
 
-obj = MyClass()
-print(obj.divide(3, 1))
+class NewClass(metaclass=DynamicNameMeta):
+    original_name = "New_Class"
+
+print(NewClass.__name__)
+
 
 
 
